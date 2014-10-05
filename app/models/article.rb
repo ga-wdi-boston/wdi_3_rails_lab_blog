0ecf1c7e1acf0fc2977fbbe67ec1f14f0417beab
fishermanswharff <fishermanswharff@github.com>
2014-10-05 11:39:34 -0400

    new layout on blog landing page, no more tables. Now posts appear in a masonry-like grid. Updates to views and forms include wysiwyg editor for creating/editing posts.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ActiveRecord::Base
+  # include Bootsy::Container
   has_many :comments, dependent: :destroy
   validates :title, presence: true, length: {minimum: 5}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141004164555) do
+ActiveRecord::Schema.define(version: 20141005040351) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
@@ -19,6 +19,36 @@ ActiveRecord::Schema.define(version: 20141004164555) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "bootsy_image_galleries", force: true do |t|
+    t.integer  "bootsy_resource_id"
+    t.string   "bootsy_resource_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "bootsy_images", force: true do |t|
+    t.string   "image_file"
+    t.integer  "image_gallery_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "ckeditor_assets", force: true do |t|
+    t.string   "data_file_name",               null: false
+    t.string   "data_content_type"
+    t.integer  "data_file_size"
+    t.integer  "assetable_id"
+    t.string   "assetable_type",    limit: 30
+    t.string   "type",              limit: 30
+    t.integer  "width"
+    t.integer  "height"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "ckeditor_assets", ["assetable_type", "assetable_id"], name: "idx_ckeditor_assetable"
+  add_index "ckeditor_assets", ["assetable_type", "type", "assetable_id"], name: "idx_ckeditor_assetable_type"
 
   create_table "comments", force: true do |t|
     t.string   "commenter"
