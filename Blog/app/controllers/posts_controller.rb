d00769d4bc49379200a953a06f3dfb65a1b8b7b0
Holly Moody <hollymoody@charter.net>
2014-10-09 19:12:46 -0400

    Add ability to create new posts.

--- Blog/app/controllers/posts_controller.rb
+++ Blog/app/controllers/posts_controller.rb
@@ -8,11 +8,25 @@ class PostsController < ApplicationController
   def show
   end
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create(post_params)
+    redirect_to post_path(@post)
+  end
+
+
   private
   def set_post
    @post = Post.find(params[:id])
    @comment = Comment.new(post_id: @post.id)
    @comments = @post.comments
+  end
+
+  def post_params
+    params.require(:post).permit(:author, :subject, :content)
   end
 
  end
