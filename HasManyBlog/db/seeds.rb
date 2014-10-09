058a5e70a6a221fc98207dcaaab10f96af92671f
Rebecca Paz <r.pazyaquian@gmail.com>
2014-10-09 14:01:23 -0400

    fixed seeds, finished working index view

--- HasManyBlog/db/seeds.rb
+++ HasManyBlog/db/seeds.rb
@@ -1,5 +1,5 @@
-post1 = Post.new(title: "hi", author: "me", content: "hello")
-post2 = Post.new(title: "bye", author: "me", content: "goodbye")
+post1 = Post.create(title: "hi", author: "me", content: "hello")
+post2 = Post.create(title: "bye", author: "me", content: "goodbye")
 
-comment1 = Comment.new(author: 'also me', content: 'what', post: post1.id)
-comment2 = Comment.new(author: 'also me', content: 'huh', post: post2.id)
+comment1 = post1.comments.create(author: 'also me', content: 'what')
+comment2 = post2.comments.create(author: 'also me', content: 'huh')
