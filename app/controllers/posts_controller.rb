412be77ea624a567b43ac50bedb08ed07a26e154
shaynaCummings <shayna.cummings@gmail.com>
2014-10-09 15:25:36 -0400

    index and show are functional

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,14 @@
 class PostsController < ApplicationController
 
   def index
+    @posts = Post.all
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
+
+private
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,15 @@
 
-post1 = Post.create(post_title: "best day ever", post_content: "we went to the zoo!", post_user: "Shayna")
+post1 = Post.create(post_title: "best day ever", post_content: "we went to the zoo!", post_user: "Shayna", :post_timestamp)
 
 
-post2 = Post.create(post_title: "worst day ever", post_content: "I fell down the stairs", post_user: "Shayna")
+post2 = Post.create(post_title: "worst day ever", post_content: "I fell down the stairs", post_user: "Shayna", :post_timestamp)
 
 
-post1 = Post.create(post_title: "life is rad", post_content: "ice cream is my favorite", post_user: "Shayna")
+post1 = Post.create(post_title: "life is rad", post_content: "ice cream is my favorite", post_user: "Shayna", :post_timestamp)
 
-comment1 = Comment.create(comment_text: "woohoo", comment_user: "sean", post_id: 1)
+comment1 = Comment.create(comment_text: "woohoo", comment_user: "sean", post_id: 1, :comment_timestamp)
 
-comment2 = Comment.create(comment_text: "that sucks", comment_user: "anna", post_id: 2)
+comment2 = Comment.create(comment_text: "that sucks", comment_user: "anna", post_id: 2, :comment_timestamp)
 
 
 # This file should contain all the record creation needed to seed the database with its default values.
