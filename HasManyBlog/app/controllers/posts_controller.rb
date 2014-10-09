c9a17f1ee204cbe6e4388b6e1bb59bacd8e08bb7
Rebecca Paz <r.pazyaquian@gmail.com>
2014-10-09 14:15:29 -0400

    finished create post functionality, including comments in post view

--- HasManyBlog/app/controllers/posts_controller.rb
+++ HasManyBlog/app/controllers/posts_controller.rb
@@ -4,8 +4,19 @@ class PostsController < ApplicationController
   end
   def show
     @post = Post.find(params[:id])
+    @comment = @post.comments.new
   end
   def new
     @post = Post.new
   end
+  def create
+    @post = Post.create(post_params)
+    redirect_to @post
+  end
+
+  private
+
+    def post_params
+      params.require(:post).permit(:title, :author, :content)
+    end
 end
