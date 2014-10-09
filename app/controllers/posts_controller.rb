3b54d5b0cff9b095edb921033385f637b864f581
shaynaCummings <shayna.cummings@gmail.com>
2014-10-09 16:27:28 -0400

    omg everything works

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,57 @@
 class PostsController < ApplicationController
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
+
 
   def index
-    @posts = Post.all
+    @posts = Post.all.order(post_time: :asc)
   end
 
   def show
     @post = Post.find(params[:id])
   end
 
+  def new
+    @post = Post.new
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post, notice: "created!"
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to @post, notice: "updated!"
+    else
+      render :edit
+    end
+  end
+
+
+  def destroy
+    @post.destroy
+    redirect_to posts_path, notice: "baleted!"
+  end
+
 
 private
+# will refactor more with this later
+  def find_post
+    @post = Post.find(params[:id])
+  end
+
+
+  def post_params
+    params.require(:post).permit(:post_title, :post_content, :post_user, :post_time)
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :posts, only: [:index, :show]
+  resources :posts
   resources :comments
   root 'posts#index'
 
