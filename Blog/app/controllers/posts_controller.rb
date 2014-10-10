92cab54b9b84eb4dbf9f6aaf4eda553bdfe8c04e
Holly Moody <hollymoody@charter.net>
2014-10-10 07:51:48 -0400

    Add destroy functionality

--- Blog/app/controllers/posts_controller.rb
+++ Blog/app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update]
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
     @posts = Post.order(created_at: :desc)
@@ -22,12 +22,16 @@ class PostsController < ApplicationController
   end
 
   def update
-    binding.pry
     if @post.update(post_params)
       redirect_to @post, notice: 'Post was successfully updated.'
     else
       render :edit
     end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_path, notice: 'post was successfully deleted'
   end
 
 
